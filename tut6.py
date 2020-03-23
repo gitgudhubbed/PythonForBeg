e2e0c0152fcd3414729ba8a74eeb056cede09fb0
Tim Roberts <t.roberts8@uni.brighton.ac.uk>
2020-03-23 12:28:20 +0000

    Multiplcation function for testing

---
+++ tut6.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 23 12:18:37 2020
+
+@author: Tim
+"""
+
+#Function to take 2 inputs and multiply them together
+a = int(input("Enter a number: "))
+b = int(input("Enter another number: "))
+c = a * b
+print (c)
